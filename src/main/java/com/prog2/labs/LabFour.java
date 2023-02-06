a0b9280ae7186ad39440762c424c36512a99a676
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2023-02-06 12:12:18 -0500

    Update LabFour.java

--- src/main/java/com/prog2/labs/LabFour.java
+++ src/main/java/com/prog2/labs/LabFour.java
@@ -15,31 +15,12 @@ public class LabFour {
 	}
 	
 	/**
-	 * 1) You are given an integer array height of length n. There are n vertical lines 
-	 * drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]). 
-	 * Find two lines that together with the x-axis form a container, such that 
-	 * the container contains the most water. Return the maximum amount of water 
-	 * a container can store. Notice that you may not slant the container.
-	 * 
-	 * @param height the input array
-	 *
-	 * @return result the maximum amount of water a container can store.
+	 * Please refer to the README file for question(s) description
 	 */
 	
     public int containerWithMostWater(int[] height) {
         return -1;
     }
-    
-	/**
-	 * 2) Given an integer array nums of length n and an integer target, find three integers
-	 * in nums such that the sum is closest to target. Return the sum of the three integers.
-	 * You may assume that each input would have exactly one solution.
-	 * 
-	 * @param nums the input array.
-	 * @param target the target number.
-	 *
-	 * @return result the closest number to sum of 3.
-	 */
     
     public int threeSumClosest(int[] nums, int target) {
         return -1;
